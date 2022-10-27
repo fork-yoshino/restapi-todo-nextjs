f8ed09636e06a379a8473b2531d2d44e95b70004
Yuui Yoshino <yoshino@fork.co.jp>
2022-10-27 22:47:52 +0900

    Chore ホットリロードの設定を追加

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // Enable Hot Reload
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
